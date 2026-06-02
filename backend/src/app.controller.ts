b5e055b00167dc966c371535517b6ef51b979ef7
insu <hinsu93@gmail.com>
2026-06-02 21:20:48 +0900

    [BE] Prisma generate Test

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { prisma } from './lib/prisma';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,12 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    prisma.test.create({
+      data: {
+        id: '1',
+      },
+    });
+
     return this.appService.getHello();
   }
 }
