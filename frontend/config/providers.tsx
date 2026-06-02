00f2b9d904829caa703082d1026d75b36ad1803e
insu <hinsu93@gmail.com>
2026-06-03 04:02:04 +0900

    [FE] Provider 추가

---
+++ frontend/config/providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Provider as JotaiProvider } from "jotai";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: React.PropsWithChildren) {
+  return (
+    <JotaiProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </JotaiProvider>
+  );
+}
